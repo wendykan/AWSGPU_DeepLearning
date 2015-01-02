38644ce4b2ff95beed69ca57976d7e3bff2a1008
wendy <wendykan@gmail.com>
2015-01-02 14:51:35 -0800

    Update kfkd.py

--- kfkd.py
+++ kfkd.py
@@ -6,8 +6,8 @@ from pandas.io.parsers import read_csv
 from sklearn.utils import shuffle
 
 
-FTRAIN = '~/data/training.csv'
-FTEST = '~/data/test.csv'
+FTRAIN = '/Users/wendy/Documents/comps/facial_keypoints/data/training.csv'
+FTEST = '/Users/wendy/Documents/comps/facial_keypoints/data/test.csv'
 
 
 def load(test=False, cols=None):
@@ -42,8 +42,78 @@ def load(test=False, cols=None):
     return X, y
 
 
-X, y = load()
-print("X.shape == {}; X.min == {:.3f}; X.max == {:.3f}".format(
-    X.shape, X.min(), X.max()))
-print("y.shape == {}; y.min == {:.3f}; y.max == {:.3f}".format(
-    y.shape, y.min(), y.max()))
+from lasagne import layers
+from lasagne.updates import nesterov_momentum
+from nolearn.lasagne import NeuralNet
+
+net1 = NeuralNet(
+    layers=[  # three layers: one hidden layer
+        ('input', layers.InputLayer),
+        ('hidden', layers.DenseLayer),
+        ('output', layers.DenseLayer),
+        ],
+    # layer parameters:
+    input_shape=(128, 9216),  # 128 images per batch times 96x96 input pixels
+    hidden_num_units=100,  # number of units in hidden layer
+    output_nonlinearity=None,  # output layer uses identity function
+    output_num_units=30,  # 30 target values
+
+    # optimization method:
+    upate=nesterov_momentum,
+    update_learning_rate=0.01,
+    update_momentum=0.9,
+
+    regression=True,  # flag to indicate we're dealing with regression problem
+    max_epochs=400,  # we want to train this many epochs
+    verbose=1,
+    )
+
+#X, y = load()
+#net1.fit(X, y)
+
+def load2d(test=False, cols=None):
+    X, y = load(test=test)
+    X = X.reshape(-1, 1, 96, 96)
+    return X, y
+
+
+# use the cuda-convnet implementations of conv and max-pool layer
+Conv2DLayer = layers.cuda_convnet.Conv2DCCLayer
+MaxPool2DLayer = layers.cuda_convnet.MaxPool2DCCLayer
+
+net2 = NeuralNet(
+    layers=[
+        ('input', layers.InputLayer),
+        ('conv1', Conv2DLayer),
+        ('pool1', MaxPool2DLayer),
+        ('conv2', Conv2DLayer),
+        ('pool2', MaxPool2DLayer),
+        ('conv3', Conv2DLayer),
+        ('pool3', MaxPool2DLayer),
+        ('hidden4', layers.DenseLayer),
+        ('hidden5', layers.DenseLayer),
+        ('output', layers.DenseLayer),
+        ],
+    input_shape=(128, 1, 96, 96),
+    conv1_num_filters=32, conv1_filter_size=(3, 3), pool1_ds=(2, 2),
+    conv2_num_filters=64, conv2_filter_size=(2, 2), pool2_ds=(2, 2),
+    conv3_num_filters=128, conv3_filter_size=(2, 2), pool3_ds=(2, 2),
+    hidden4_num_units=500, hidden5_num_units=500,
+    output_num_units=30, output_nonlinearity=None,
+
+    update_learning_rate=0.01,
+    update_momentum=0.9,
+
+    regression=True,
+    max_epochs=1000,
+    verbose=1,
+    )
+
+X, y = load2d()  # load 2-d data
+net2.fit(X, y)
+
+# Training for 1000 epochs will take a while.  We'll pickle the
+# trained model so that we can load it back later:
+import cPickle as pickle
+with open('net2.pickle', 'wb') as f:
+    pickle.dump(net2, f, -1)
