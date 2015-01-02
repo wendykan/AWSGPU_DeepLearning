81899c1cdd55cf7741a107a6fd6520ba82bf877d
wendy <wendykan@gmail.com>
2015-01-02 15:23:53 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,6 +15,7 @@ wget -x --load-cookies ~/cookies.txt https://www.kaggle.com/c/facial-keypoints-d
 wget -x --load-cookies ~/cookies.txt https://www.kaggle.com/c/facial-keypoints-detection/download/test.zip
 wget -x --load-cookies ~/cookies.txt https://www.kaggle.com/c/facial-keypoints-detection/download/SampleSubmission.csv
 wget -x --load-cookies ~/cookies.txt https://www.kaggle.com/c/facial-keypoints-detection/download/IdLookupTable.csv
+mv www.kaggle.com/c/facial-keypoints-detection/download/* .
 sudo apt-get install unzip
 unzip training.zip
 unzip test.zip
