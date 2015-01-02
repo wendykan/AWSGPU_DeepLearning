bfbc5a124348f5125e659b8f045d66f84a0b2a8f
wendy <wendykan@gmail.com>
2015-01-02 14:52:54 -0800

    Update kfkd.py

--- kfkd.py
+++ kfkd.py
@@ -6,8 +6,8 @@ from pandas.io.parsers import read_csv
 from sklearn.utils import shuffle
 
 
-FTRAIN = '/Users/wendy/Documents/comps/facial_keypoints/data/training.csv'
-FTEST = '/Users/wendy/Documents/comps/facial_keypoints/data/test.csv'
+FTRAIN = '~/data/training.csv'
+FTEST = '~/data/test.csv'
 
 
 def load(test=False, cols=None):
